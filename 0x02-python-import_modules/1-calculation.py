c9bdfb7141793fd027d14bf2ca75acb144ce70da
iAmG-r00t <numimickey2@gmail.com>
2021-09-09 18:48:45 +0000

    minor edit, fix checker 🧞 👨‍💻 🦹 🔥

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, add(a, b)))
-    print("{} * {} = {}".format(a, b, add(a, b)))
-    print("{} / {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
