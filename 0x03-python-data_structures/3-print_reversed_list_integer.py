de9e6cdc0ad7e901dae2a82472bab57d2e0c07a7
iAmG-r00t <numimickey2@gmail.com>
2021-09-14 22:56:58 -0700

    checker fix 😂 👨‍ 💻 😎 ✍  👾

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,8 +3,11 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-   num = len(my_list) - 1
+    if not my_list:
+        pass
+    else:
+       num = len(my_list) - 1
 
-   while num >= 0:
-       print("{:d}".format(my_list[num]))
-       num -= 1
+       while num >= 0:
+            print("{:d}".format(my_list[num]))
+            num -= 1
