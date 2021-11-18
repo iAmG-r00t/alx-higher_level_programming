e2ceb75e776f8c6c740f09d3cc05c691beff3413
iAmG-r00t <numimickey2@gmail.com>
2021-11-18 11:08:51 +0300

    11th task checker fix 🧻 🔥 ✍  😎 😂

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with a score >= 10 in the second_table
-select score, name FROM second_table WHERE score >= '10' ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= '10' ORDER BY score DESC;
