f03cf77914d8daa5d2b1ec5f5a95bdbe278d89e2
iAmG-r00t <numimickey2@gmail.com>
2021-11-18 12:16:17 +0300

    17th tasks add reference 🧞 👨‍💻 🦹 🔥

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- converts hbtn_0c_0 database to UTF8
+-- source https://dba.stackexchange.com/questions/8239/how-to-easily-convert-utf8-tables-to-utf8mb4-in-mysql-5-5
 USE hbtn_0c_0
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
