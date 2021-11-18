90610155ddd4e5b7805950bd03c05ebc59afa5f3
iAmG-r00t <numimickey2@gmail.com>
2021-11-18 12:34:52 +0300

    18th task checker fix 🧻 🔥 ✍  😎 😂

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,6 @@
 -- import database dump temperatures.sql to hbtn_0c_0 database
 -- then lists average temp by city ordered by temp (desc)
-source temperatures.sql;
-
+-- source temperatures.sql;
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
