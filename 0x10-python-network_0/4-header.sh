bb34182db3202fa8fdf5674477aa075a1edfaa22
iAmG-r00t <numimickey2@gmail.com>
2022-01-10 08:47:31 +0300

    fix checker 🐙😀😭👌

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a GET request with header...
-# ... variable being 'X-School-User-Id' with variable 98
+# sends a GET request with header variable being 'X-School-User-Id' with variable 98
 curl -sH "X-School-User-Id: 98" "$1"
