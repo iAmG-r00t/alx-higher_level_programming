87c2b79824f060a32da7260eb79c0415e48ddaef
iAmG-r00t <numimickey2@gmail.com>
2022-01-10 20:44:26 +0300

    checker fix 🐙😀😭👌

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,10 @@ using urlib package
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as request:
         response = request.read()
         print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('utf-8')))
+        print("    - type: {}".format(type(response)))
+        print("    - content: {}".format(response))
+        print("    - utf8 content: {}".format(response.decode('utf-8')))
