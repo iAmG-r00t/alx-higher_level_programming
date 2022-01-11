db19469ac5ed777715497095d2fbc8cc3fc00062
iAmG-r00t <numimickey2@gmail.com>
2022-01-11 08:35:35 +0300

    fix checker... don't know why it never worked... 🐙😀😭👌

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-Same as Task 0
-but using request package
+Same as Task 0 but using request package
 """
 
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- type: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- type: {}".format(r.text))
