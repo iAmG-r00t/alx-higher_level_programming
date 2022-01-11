4d4701e9b5d9b118f44a61e023d600f9ea7db94a
iAmG-r00t <numimickey2@gmail.com>
2022-01-11 08:02:48 +0300

    fix checker, I don't know why it's not working... 🐙😀😭👌

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,12 @@ https://intranet.hbtn.io/status
 using urlib package
 """
 
-if __name__ == '__main__':
-    import urllib.request
+import urllib.request
 
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as request:
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
         response = request.read()
         print("Body response:")
-        print("    - type: {}".format(type(response)))
-        print("    - content: {}".format(response))
-        print("    - utf8 content: {}".format(response.decode('utf-8')))
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
