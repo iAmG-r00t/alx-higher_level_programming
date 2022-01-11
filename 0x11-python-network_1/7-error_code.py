9ed980b405c9083e2efc1e6c1face5c45fe8e3d9
iAmG-r00t <numimickey2@gmail.com>
2022-01-11 09:32:52 +0300

    fix checker... minor edit 🐙😀😭👌

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = request.get(url)
+    response = requests.get(url)
     if request.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
