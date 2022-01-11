1c37877cef3f1adb40de0fdb45cb4803d7e85d5f
iAmG-r00t <numimickey2@gmail.com>
2022-01-11 08:32:20 +0300

    fix checker... don't know why it never worked... 🐙😀😭👌

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,7 @@ import requests
 
 
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
+    response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- type: {}".format(response.text))
